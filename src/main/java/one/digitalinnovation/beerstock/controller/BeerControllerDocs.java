4bae574bc6931fb8e90144bbb08b6f4712b4d48f
DaveJosef <josedaaaaavid@gmail.com>
2022-07-18 14:31:57 -0300

    Adiciona Swagger

--- src/main/java/one/digitalinnovation/beerstock/controller/BeerControllerDocs.java
+++ src/main/java/one/digitalinnovation/beerstock/controller/BeerControllerDocs.java
@@ -1,5 +1,9 @@
 package one.digitalinnovation.beerstock.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import one.digitalinnovation.beerstock.dto.BeerDTO;
 import one.digitalinnovation.beerstock.entity.Beer;
 import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
@@ -8,13 +12,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Collection;
 
+@Api("Manages Beer Stock")
 public interface BeerControllerDocs {
 
+    @ApiOperation(value = "Beer creation operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Beer successfully created."),
+            @ApiResponse(code = 400, message = "Missing required fields or wrong field range value.")
+    })
     BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException;
 
+    @ApiOperation(value = "Returns a beer found by a given name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Beer successfully found."),
+            @ApiResponse(code = 404, message = "Beer with given Id not found.")
+    })
     Beer findByName(@PathVariable String name) throws BeerNotFoundException;
 
+    @ApiOperation(value = "Returns a list with all beers registered in the system")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "List of all beers registered in the system."),
+    })
     Collection<Beer> listBeers();
 
+    @ApiOperation(value = "Delete a beer found by a given valid Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Beer successfully deleted."),
+            @ApiResponse(code = 404, message = "Beer with given Id not found.")
+    })
     void deleteById(@PathVariable Long id) throws BeerNotFoundException;
 }
